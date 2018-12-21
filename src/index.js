89b5a95891fc4e9add03d980adfc74acc415914a
Shie0906 <shie0906@gmail.com>
2018-12-21 17:43:54 +0800

    step3：管理输出

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml'
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  // loadsh 是由当前 script 脚本 import 导入进来的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // 将图像添加到我们现有的 div
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
